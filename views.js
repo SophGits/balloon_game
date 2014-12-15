7c08640c06c04031942b06fdb0abf8825435884f
SophGits <sophie.jiffy@gmail.com>
2014-12-15 11:12:00 +0000

    Successfully set a timeout.

--- views.js
+++ views.js
@@ -26,6 +26,31 @@ app.BalloonView = Backbone.View.extend({
   },
   rise: function(){
     console.log("floating up");
+    var that = this;
+    setTimeout(function(){
+      that.$el.css('margin-top', '-60px')
+    }, 3000);
+
+
+    // $(document).ready(function() {
+    //     $('html, body').animate({scrollTop: $(document).height()-$(window).height()}, 300);
+    //   var y = '';
+    //   var x = '';
+
+    //   $(document).mousemove(function(e) {
+    //     y = e.pageY;
+    //     x = e.pageX;
+    //   }).click(function(){
+    //     $('body').append(
+    //       $('<div class="balloons"><img src="balloonpurple.png" height="100"></></div>')
+    //       .css({'top': y, 'left': x})
+    //       .animate({ top: "-=2700"}, 10000, function()
+    //         { $(this).remove();
+    //         })
+    //       );
+    //   });
+    // });
+
   }
 });
 
